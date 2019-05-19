80f172f145fb064de5e1ab55508c5b78945a98e5
Andrew Boschee <andrew.j.boschee@outlook.com>
2019-05-19 14:34:51 -0500

    Add files via upload

---
+++ AJBAlienInvaders/AJBAlienInvaders/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace AJBAlienInvaders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbOut = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSort = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSortID = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSortScore = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtbOut
+            // 
+            this.rtbOut.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbOut.Location = new System.Drawing.Point(0, 32);
+            this.rtbOut.Name = "rtbOut";
+            this.rtbOut.Size = new System.Drawing.Size(403, 447);
+            this.rtbOut.TabIndex = 0;
+            this.rtbOut.Text = "";
+            this.rtbOut.TextChanged += new System.EventHandler(this.rtbOut_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.mnuSort});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(402, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFileLoad,
+            this.mnuFileExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "File";
+            // 
+            // mnuFileLoad
+            // 
+            this.mnuFileLoad.Name = "mnuFileLoad";
+            this.mnuFileLoad.Size = new System.Drawing.Size(100, 22);
+            this.mnuFileLoad.Text = "Load";
+            this.mnuFileLoad.Click += new System.EventHandler(this.mnuFileLoad_Click);
+            // 
+            // mnuFileExit
+            // 
+            this.mnuFileExit.Name = "mnuFileExit";
+            this.mnuFileExit.Size = new System.Drawing.Size(100, 22);
+            this.mnuFileExit.Text = "Exit";
+            this.mnuFileExit.Click += new System.EventHandler(this.mnuFileExit_Click);
+            // 
+            // mnuSort
+            // 
+            this.mnuSort.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSortID,
+            this.mnuSortScore});
+            this.mnuSort.Name = "mnuSort";
+            this.mnuSort.Size = new System.Drawing.Size(40, 20);
+            this.mnuSort.Text = "Sort";
+            // 
+            // mnuSortID
+            // 
+            this.mnuSortID.Name = "mnuSortID";
+            this.mnuSortID.Size = new System.Drawing.Size(180, 22);
+            this.mnuSortID.Text = "ID";
+            this.mnuSortID.Click += new System.EventHandler(this.mnuSortID_Click);
+            // 
+            // mnuSortScore
+            // 
+            this.mnuSortScore.Name = "mnuSortScore";
+            this.mnuSortScore.Size = new System.Drawing.Size(180, 22);
+            this.mnuSortScore.Text = "Score";
+            this.mnuSortScore.Click += new System.EventHandler(this.mnuSortScore_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 476);
+            this.Controls.Add(this.rtbOut);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alien Invaders";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbOut;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileLoad;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileExit;
+        private System.Windows.Forms.ToolStripMenuItem mnuSort;
+        private System.Windows.Forms.ToolStripMenuItem mnuSortID;
+        private System.Windows.Forms.ToolStripMenuItem mnuSortScore;
+    }
+}
+
---
+++ AJBAlienInvaders/AJBAlienInvaders/Form1.cs
@@ -0,0 +1,163 @@
+﻿/*****************************************************************
+ * Andrew Boschee
+ * CIS 251: BAP
+ * Dr. Mckeown
+ * Sort Records
+ * AlienInvasion.dat file
+ * File Structure: ID, Score
+ * Loads arrays from data file and display them
+ * Sort id ascending and scores descending from menu buttons
+ * ***************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AJBAlienInvaders
+{
+    public partial class Form1 : Form
+    {
+        //declare variables
+        String[] ID = new string[1];
+        double[] score = new double[1];
+        
+        int num = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }  
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                header();
+                //IO initialization
+                string path = @"AlienInvaders.dat";
+                StreamReader textIn = new StreamReader(
+                new FileStream(path, FileMode.Open, FileAccess.Read));
+                //disable menu buttons until file read
+                mnuSortID.Enabled = false;
+                mnuSortScore.Enabled = false;
+
+                //Processing loop
+                while (textIn.Peek() != -1)
+                {
+                    //Resize arrays to make room for new record
+                    Array.Resize<string>(ref ID, ID.Length + 1);
+                    Array.Resize<double>(ref score, score.Length + 1);
+                    //Read record and add to array
+                    string row = textIn.ReadLine();
+                    string[] record = row.Split(',');
+                    ID[num] = record[0];
+                    score[num] = Convert.ToDouble(record[1]);
+                    num++;
+                }
+
+                //Close file
+                textIn.Close();
+                //Resize arrays to remove extra space
+                Array.Resize<string>(ref ID, ID.Length - 1);
+                Array.Resize<double>(ref score, score.Length - 1);
+            
+
+        }
+        private void mnuFileLoad_Click(object sender, EventArgs e)
+        {
+            rtbOut.Clear();
+            header();
+
+            //loop through data file and show all data
+            for (int i=0; i <= ID.GetUpperBound(0); i++)
+            {
+                rtbOut.AppendText(ID[i].PadRight(25) + score[i].ToString("n0").PadLeft(10) + "\n");
+            }
+            //enable/disable menu buttons
+            mnuFileLoad.Enabled = false;
+            mnuSortID.Enabled = true;
+            mnuSortScore.Enabled = true;
+        }
+        //header procedure
+        private void header()
+        {
+            rtbOut.AppendText("          Alien Invaders" + "\n");
+            rtbOut.AppendText("      All-Time Scoring Leaders " + "\n\n");
+            rtbOut.AppendText("ID                           Score" + "\n");
+        }
+
+        
+
+        private void rtbOut_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mnuFileExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void mnuSortID_Click(object sender, EventArgs e)
+        {
+            //sort ids in ascending order
+            for(int i=0; i < ID.GetUpperBound(0); i++)
+            {
+                for(int j=0; j < (ID.GetUpperBound(0)-i); j++)
+                {
+                    int k = j + 1;
+
+                    if (string.Compare(ID[j], ID[k]) >= 0)
+                    {
+                        string tempID = ID[j];
+                        ID[j] = ID[k];
+                        ID[k] = tempID;
+                        double tempScore = score[j];
+                        score[j] = score[k];
+                        score[k] = tempScore;
+                    }
+                }
+            }
+            rtbOut.Clear();
+            header();
+            //loop through sorted ids
+            for(int i =0; i <= ID.GetUpperBound(0); i++)
+            {
+                rtbOut.AppendText(ID[i].PadRight(25) + score[i].ToString("n0").PadLeft(10) + "\n");
+            }
+        }
+
+        private void mnuSortScore_Click(object sender, EventArgs e)
+        {
+            //sort scores by descending order
+            for (int i = 0; i < ID.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < (ID.GetUpperBound(0)-i); j++)
+                {
+                    int k = j + 1;
+
+                    if (score[j]< score[k])
+                    {
+                        double tempScore = score[j];
+                        score[j] = score[k];
+                        score[k] = tempScore;
+                        string tempID = ID[j];
+                        ID[j] = ID[k];
+                        ID[k] = tempID;
+
+                    }
+                }
+            }
+            rtbOut.Clear();
+            header();
+            //loop through sorted scores
+            for (int i = 0; i <= ID.GetUpperBound(0); i++)
+            {
+                rtbOut.AppendText(ID[i].PadRight(25) + score[i].ToString("n0").PadLeft(10) + "\n");
+            }
+        }
+    }
+}
